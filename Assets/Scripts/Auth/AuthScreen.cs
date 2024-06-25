b9d52eb9812d53aa38a7bae9cf412e00c405eabf
Brandon <brandonplokker@live.nl>
2024-06-25 20:15:39 +0200

    feat(auth): added auth instructions

--- Assets/Scripts/Auth/AuthScreen.cs
+++ Assets/Scripts/Auth/AuthScreen.cs
@@ -2,6 +2,15 @@ using UnityEngine;
 
 public class AuthScreen : MonoBehaviour
 {
+    public GameObject WelcomeContent;
+    public GameObject LoginContent;
+
+    public void GetStarted()
+    {
+        WelcomeContent.SetActive(false);
+        LoginContent.SetActive(true);
+    }
+
     public void CloseApp()
     {
         Application.Quit();
